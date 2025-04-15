7e68309222d33729177335b8ad91e895d6ac5cd3
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-15 23:06:30 +0300

    new metric api added

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { CompanyProfile, CompanySearch } from "./company";
+import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
@@ -28,6 +28,17 @@ export const getCompanyProfile = async (query: string) => {
     const data = await axios.get<CompanyProfile[]>(
       `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
       //`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}` Old version.
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message from API: ", error.message);
+  }
+};
+export const getKeyMetrics = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyKeyMetrics[]>(
+      //`https://financialmodelingprep.com/stable/key-metrics-ttm?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}` // need to change company.d.ts "CompanyKeyMetrics"
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}` //Old version.
     );
     return data;
   } catch (error: any) {
