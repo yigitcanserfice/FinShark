e9ab2211d4f56937bfdbcb5e8b90e88a7d3ee8e1
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-16 23:47:29 +0300

    design changes

--- frontend/src/Components/CompanyDashboard/CompanyDasboard.tsx
+++ frontend/src/Components/CompanyDashboard/CompanyDasboard.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import Tile from "../Tile/Tile";
 import { Outlet } from "react-router-dom";
-import { ChildProcess } from "child_process";
 
-type Props = {
+interface Props {
   children: React.ReactNode;
   ticker: string;
-};
+}
 
-const CompanyDasboard = ({ children, ticker }: Props) => {
+const CompanyDashboard = ({ children, ticker }: Props) => {
   return (
-    <div>
-      <div className="relative md:ml-64 bg-blueGray-100 w-full">
-        <div className="relative pt-20 pb-32 bg-lightBlue-500">
-          <div className="px-4 md:px-6 mx-auto w-full">
-            <div>
-              <div className="flex flex-wrap">{children}</div>
-              <div className="flex flex-wrap">
-                {<Outlet context={ticker} />}
-              </div>
-            </div>
+    <div className="relative md:ml-64 bg-blueGray-100 w-full">
+      <div className="relative pt-20 pb-32 bg-lightBlue-500">
+        <div className="px-4 md:px-6 mx-auto w-full">
+          <div>
+            <div className="flex flex-wrap">{children}</div>
+            <div className="flex flex-wrap">{<Outlet context={ticker} />}</div>
           </div>
         </div>
       </div>
@@ -27,4 +21,4 @@ const CompanyDasboard = ({ children, ticker }: Props) => {
   );
 };
 
-export default CompanyDasboard;
+export default CompanyDashboard;
