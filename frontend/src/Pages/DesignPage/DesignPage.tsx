24ad6acf49ff5324a4dea79dafce87640f05930a
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-16 23:48:09 +0300

    table props added

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -22,7 +22,7 @@ const DesignPage = (props: Props) => {
         design aspects of the app.
       </h2>
       <RatioList data={testIncomeStatementData} config={tableConfig} />
-      <Table />
+      <Table data={testIncomeStatementData} config={tableConfig} />
     </>
   );
 };
