c63571349242a254ac0ed9f86d2848b6bc73a735
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-20 14:43:09 +0300

    style updates

--- frontend/src/Components/RatioList/RatioList.tsx
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -4,19 +4,25 @@ type Props = {
 };
 
 const RatioList = ({ config, data }: Props) => {
-  const renderedRows = config.map((row: any) => {
+  const renderedCells = config.map((row: any) => {
     return (
-      <li className=" py-3 sm:py-4 ">
+      <li className="py-6 sm:py-6">
         <div className="flex items-center space-x-4">
           <div className="flex-1 min-w-0">
             <p className="text-sm font-medium text-gray-900 truncate">
               {row.label}
             </p>
-            <p className=" text-sm text-gray-500 truncate">
-              {row.subTitle && row.subTitle}
+            <p className="text-sm text-gray-500 truncate">
+              <a
+                href="/cdn-cgi/l/email-protection"
+                className="__cf_email__"
+                data-cfemail="17727a767e7b57607e7973646372653974787a"
+              >
+                {row.subTitle && row.subTitle}
+              </a>
             </p>
           </div>
-          <div className=" inline-flex items-center text-base font-semibold text-gray-900">
+          <div className="inline-flex items-center text-base font-semibold text-gray-900">
             {row.render(data)}
           </div>
         </div>
@@ -24,8 +30,8 @@ const RatioList = ({ config, data }: Props) => {
     );
   });
   return (
-    <div className=" bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 h-full">
-      <ul className=" divide-y divide-gray-200">{renderedRows}</ul>
+    <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 w-full">
+      <ul className="divide-y divide-gray-200">{renderedCells}</ul>
     </div>
   );
 };
