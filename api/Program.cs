31517d4182e93ab403c67863c42e139dc623ee51
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-22 17:35:14 +0300

    first commit

---
+++ api/Program.cs
@@ -0,0 +1,26 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger/OpenAPI servislerini ekle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Geliştirme ortamında Swagger UI'ı aktif et
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
