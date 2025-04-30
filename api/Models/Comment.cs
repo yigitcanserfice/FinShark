2e03c5dad36474cf5b1622cdc476b1e1e5f2676d
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-04-30 10:07:16 +0300

    ManyToMany for Portfolios

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.Models
 {
+    [Table("Comments")]
     public class Comment
     {
-
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
