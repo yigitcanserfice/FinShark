836d73abe2dff787b30617975253a4536902ecae
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-06-17 13:25:57 +0300

    Fix portfolio addition response status check to 201

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -47,7 +47,7 @@ const SearchPage = (props: Props) => {
     e.preventDefault();
     portfolioAddAPI(e.target[0].value)
       .then((res) => {
-        if (res?.status === 204) {
+        if (res?.status === 201) {
           toast.success("Stock added to portfolio!");
           getPortfolio();
         }
